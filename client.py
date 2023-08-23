ad05077c2b382c9ec279988fb0c419a92df0f136
kishore5214715 <139248946+kishore5214715@users.noreply.github.com>
2023-08-23 11:48:01 +0530

    First commit

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+
+
+Head=64
+port = 5000
+disconnect = "!DISCONNECT"
+format = "utf-8"
+SERVER ="192.168.188.1"
+ADDR=(SERVER,port)
+
+client =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(format)
+    msg_length=len(message)
+    send_length =str(msg_length).encode(format)
+    send_length += b' '* (Head -len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send("hello1")
+send("hello2")
+send("hello3")
+send("hello4")
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+import time
+
+Head=64
+port = 5000
+disconnect = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname()) #TO get the host ip address
+format = "utf-8"
+
+acces = (SERVER,port)
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#AF_INET is a ipv4 address and sock_stream to get the stream of data sharing
+
+server.bind(acces)
+
+
+def client(conn,addr):
+    print(f"[NEW CONNECTION ]{addr} connected")
+    connection=True
+    while connection:
+        msg_len = conn.recv(Head).decode(format)
+        if msg_len:
+            msg_len = int(msg_len)
+            msg=conn.recv(msg_len).decode(format)
+            print(f"[{addr}] {msg}")
+            if msg == disconnect:
+                connection = False
+            print(f"[{addr}] {msg}")
+    conn.close()
+
+
+
+def start():
+    server.listen()
+    print(f"the ip address listen is {SERVER}")
+    while True:
+        conn,addr = server.accept()
+        thread = threading.Thread(target=client,args=(conn,addr))
+        thread.start()
+        print(f'active connection{threading.active_count()-1}')
+
+print("the server is starting")
+start()
